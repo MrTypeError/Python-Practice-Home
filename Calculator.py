0abd8472d1b525dd28f7d070aed3f84161a8dac3
MrTypeError <sanugta8@gmail.com>
2023-03-05 21:47:09 +0530

    Stored The Entered Charecter In Broken Form

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,9 @@
+# Initial Thinking By eval 
+
+'''We have not implimented that because it is 
+very prone to errors and is not good with respect to security 
+'''
+
 # def Calculator(expression):
 #     result = eval(expression)
 #     return result
@@ -5,3 +11,12 @@
 
 # expression = input("Enter a Number :-")
 # Calculator(expression)
+
+
+# Another Approach 
+result = [] 
+expression = input()
+
+storage_list = list(expression)
+# storage_list.append()
+print(storage_list)
